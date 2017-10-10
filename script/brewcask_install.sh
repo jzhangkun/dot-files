db6003770f1d84d551081689d556de24e6a4e28b
jzhangkun <jzhangkun@jzhangkuns-MacBook-Pro.local>
2017-10-10 21:55:51 +0800

    brew cask install softwares in mac

---
+++ script/brewcask_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+brew cask install iterm2
+brew cask install alfred
+#brew cask install the-unarchiver
+brew cask install qq
+#brew cask install line
+brew cask install skype
+brew cask install thunder
+brew cask install mplayerx
+brew cask install evernote
+brew cask install skitch
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install mou
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install launchrocket
+
+# intellij idea commercial & community
+#brew cask install intellij-idea
+brew cask install intellij-idea-ce
+
+# android phone
+brew cask install android-file-transfer
+
+# mysqlbench
+brew cask install mysqlworkbench
